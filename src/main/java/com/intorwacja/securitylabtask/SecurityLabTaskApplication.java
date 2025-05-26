dc7c8fbb0daf87269c469916734168f2ea2fa47f
intorwacja <dominik.wasniewski@gmail.com>
2025-05-26 21:29:29 +0200

    initial commit

---
+++ src/main/java/com/intorwacja/securitylabtask/SecurityLabTaskApplication.java
@@ -0,0 +1,13 @@
+package com.intorwacja.securitylabtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLabTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityLabTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intorwacja/securitylabtask/SecurityLabTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intorwacja.securitylabtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLabTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
